85cdf13af22dfbc712b1d48a072b97c660c15000
Wolpz <laurensveld@protonmail.com>
2023-09-10 16:19:05 +0200

    Completed assignment 1

--- templates/Opdracht1/opdracht1.php
+++ templates/Opdracht1/opdracht1.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Opdracht 1</title>
+</head>
+<body>
+<?php
+$text = array("Wordt", "dit", "nou", "steeds", "groter?");
+for ($i=0; $i<count($text); $i++){
+    $fsize = (($i+1)*10)."px";
+    if($i%2){
+        echo "<div style='font-size:$fsize;'>". $text[$i] ."</div>";
+    }
+    else {
+        echo "<div style='font-size:$fsize;'><i>". $text[$i] . "</i> </div>";
+    }
+}
+?>
+</body>
+</html>
