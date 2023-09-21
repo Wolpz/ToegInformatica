7a63531b8a4352ff4e16fdf7971265364533f30c
Wolpz <laurensveld@protonmail.com>
2023-09-21 13:23:57 +0200

    finished 6d, testing 6e

--- templates/index.php
+++ templates/index.php
@@ -9,5 +9,11 @@
 <a href="2/opdracht2.php">Opdracht 2</a><br>
 <a href="3/opdracht3b.php">Opdracht 3b</a><br>
 <a href="3/opdracht3c.php">Opdracht 3c</a><br>
+<a href="4/opdracht4a.php">Opdracht 3c</a><br>
+<a href="4/opdracht4b.php">Opdracht 3c</a><br>
+<a href="4/opdracht4c.php">Opdracht 3c</a><br>
+<a href="5/opdracht5c.php">Opdracht 3c</a><br>
+<a href="6/opdracht6d.php">Opdracht 3c</a><br>
+<a href="6/opdracht6e.php">Opdracht 3c</a><br>
 </body>
 </html>
