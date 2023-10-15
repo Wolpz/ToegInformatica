077b1da6ab35f01eb16289cf003da8b6871da8cf
Wolpz <laurensveld@protonmail.com>
2023-10-15 15:19:39 +0200

    finished 7a

--- templates/7/opdracht7a.php
+++ templates/7/opdracht7a.php
@@ -1,35 +1,64 @@
 <?php
-if (isset($_REQUEST['username']))
-    $user=$_REQUEST["username"];
+if (isset($_POST['username']))
+    $user=$_POST["username"];
 else
     $user="";
-if (isset($_REQUEST['password']))
-    $password=$_REQUEST["password"];
+if (isset($_POST['password']))
+    $password=$_POST["password"];
 else
     $password="";
 ?>
+
 <html>
 <head>
     <title>Inloggen</title>
 </head>
 <body>
-Voer uw gegevens in:<br>
-<form name=aanmelden method="post">
+<?php
+$loginpage = true;
+ if( ($user == "") || ($password == "") ){
+     // If password or user empty
+     echo 'Vul alstublieft een username en wachtwoord in.<br>';
+     $loginpage = true;
+ }
+ else if(FindInFile($user, '../../DATA/o7usernames.txt', false) > -1){
+     if (isset($_POST["nw"])) {
+         echo 'Kies een unieke username. Deze bestaat al.<br>';
+         $loginpage = true;
+     } else if (isset($_POST['log'])) {
+         if (getLineFromFile(FindInFile($user, '../../DATA/o7usernames.txt', false), '../../DATA/o7passwords.txt') == crypt($password, $user)) {
+             echo 'Welkom ' . $user . '!<br>';
+             $loginpage = false;
+         } else {
+             echo 'Fout wachtwoord. <br>';
+             $loginpage = true;
+         }
+     }
+ }
+else if(FindInFile($user, '../../DATA/o7usernames.txt', false) == -1){
+    if(isset($_POST["nw"])){
+            addNewUser($user, $password);
+            echo 'U heeft succesvol een nieuwe account aangemaakt.<br>';
+            $loginpage = false;
+        }
+    else{
+        echo 'User niet gevonden.<br>';
+    }
+ }
+
+ if($loginpage){
+     echo 'Voer uw gegevens in:<br>
+<form name="aanmelden" method="post">
     <input type="text" name="username" value="">
     <input type="password" name="password">
     <input type="submit" value="Login" name="log">
     <input type="submit" value="Nieuw" name="nw">
-</form>
-
-<?php
- if( ($user == "") || ($password == "") ){
-     // If password or user empty
-     echo 'Vul alstublieft een username en wachtwoord in.<br>';
+</form>';
  }
- else if()
-
- else
-
+ else{
+     echo 'U bent ingelogd!<br>';
+     echo '<img src="../../images/tree-frog.jpg" width = 50%>';
+ }
 ?>
 
 </body>
@@ -37,21 +66,23 @@ Voer uw gegevens in:<br>
 
 <?php
 // Appends databases with new user and hashed+salted password. Returns 1 if successful and 0 if not.
-function addNewUser($user, $pw, $db){
-    return appendFile($user, 'o7usernames.txt') &&
-        appendFile(crypt($pw, $user), 'o7passwords.txt');
+function addNewUser($user, $pw){
+    return appendFile($user, '../../DATA/o7usernames.txt') &&
+        appendFile(crypt($pw, $user), '../../DATA/o7passwords.txt');
 }
 
 // Appends at end of file in DATA directory if it exists. Returns 1 if successful, 0 if file does not exist.
 function appendFile($string, $file){
-    if(!file_exists('../DATA/'.$file))
+    if(!file_exists($file))
         return 0;
 
-    $feed = fopen('../DATA/'.$file, 'a');
-    fwrite($feed, $string.'\n');
+    $feed = fopen($file, 'a');
+    fwrite($feed, $string."\n");
     return 1;
 }
 
+
+// 502 ERROR IS HERE SOMEWHERE
 // Returns -1 if string is not found in file, or position of string if found (starting at 0)
 function FindInFile($findString, $filepath, $IGNORECASE = false){
     if($IGNORECASE)
@@ -79,10 +110,10 @@ function FindInFile($findString, $filepath, $IGNORECASE = false){
 function getLineFromFile($linenum, $filepath){
     $buffer = '';
     $fd = fopen($filepath,"r");
-    for($i = 0; ($i <= $linenum || feof($fd)); $i++){
-        $buffer=strtolower(fgets($fd,4096));
+    for($i = 0; ($i <= $linenum && !feof($fd)); $i++){
+        $buffer=fgets($fd,4096);
     }
     fclose($fd);
-    return $buffer;
+    return trim($buffer);
 }
 ?>
